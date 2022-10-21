7c31692c3173f4444b0830970d4798b9b2a9d067
Vincent Fiduccia <vincent@acorn.io>
2022-10-21 12:28:25 -0700

    Kubeconfig, download main

--- src/acorn.ts
+++ src/acorn.ts
@@ -3,12 +3,14 @@ import * as exec from '@actions/exec'
 import * as tc from '@actions/tool-cache'
 import fetch from 'node-fetch'
 import os from 'os'
+import {mkdtempSync, chmodSync} from 'fs'
 
 interface Asset {
   version: string
   platform: string
   arch: string
   url: string
+  extension: string
 }
 
 export async function getChannels(): Promise<Record<string, string>> {
@@ -27,6 +29,10 @@ export async function getChannels(): Promise<Record<string, string>> {
 }
 
 export async function resolveVersion(input: string): Promise<string> {
+  if (input === 'main') {
+    return input
+  }
+
   if (input.match(/^\d+/)) {
     return `v${input}`
   } else if (input.match(/^v\d+/)) {
@@ -49,33 +55,62 @@ export async function resolveVersion(input: string): Promise<string> {
 
 export async function resolveAsset(version: string): Promise<Asset> {
   let platform = `${os.platform()}`
-  let extension = 'tar.gz'
   let arch = os.arch()
-
-  if (platform === 'darwin') {
-    platform = 'macos'
-    arch = 'universal'
-  } else if (platform === 'windows') {
-    extension = 'zip'
-  }
+  let extension = ''
+  let url = ''
 
   if (arch === 'x64') {
     arch = 'amd64'
   }
 
-  const url = `https://github.com/acorn-io/acorn/releases/download/${version}/acorn-${version}-${platform}-${arch}.${extension}`
+  if (version === 'main') {
+    if (platform === 'windows') {
+      extension = 'exe'
+    }
+
+    url = `https://cdn.acrn.io/cli/default_${platform}_${arch}${arch === 'amd64' ? '_v1' : ''}/acorn`
+  } else {
+    switch (platform) {
+      case 'windows':
+        extension = 'zip'
+        break
+      case 'darwin':
+        platform = 'macos'
+        arch = 'universal'
+        extension = 'tar.gz'
+        break
+      case 'linux':
+        extension = 'tar.gz'
+    }
+
+    url = `https://github.com/acorn-io/acorn/releases/download/${version}/acorn-${version}-${platform}-${arch}`
+  }
+
+  if (extension) {
+    url = `${url}.${extension}`
+  }
 
   return Promise.resolve({
     version,
     platform,
     arch,
+    extension,
     url
   } as Asset)
 }
 
 export async function installAsset(asset: Asset): Promise<void> {
-  const downloaded = await tc.downloadTool(asset.url)
-  const dir = await tc.extractTar(downloaded, '')
+  let dir = ''
+
+  if (asset.extension && asset.extension !== 'exe') {
+    const downloaded = await tc.downloadTool(asset.url)
+    dir = await tc.extractTar(downloaded, '')
+  } else {
+    dir = mkdtempSync('acorn-')
+    const file = `${dir}/acorn${asset.extension === '.exe' ? '.exe' : ''}`
+    await tc.downloadTool(asset.url, file)
+    chmodSync(file, '775')
+  }
 
   const cachedPath = await tc.cacheDir(dir, 'acorn', asset.version)
   core.addPath(cachedPath)
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,26 @@
 import * as core from '@actions/core'
 import * as acorn from './acorn'
 import * as k3s from './k3s'
+import {writeFileSync, existsSync, mkdirSync} from 'fs'
+import {resolve} from 'path'
 
 async function setup(): Promise<void> {
-  if (core.getBooleanInput('k3s-install')) {
+  const kubeconfig = core.getInput('kubeconfig')
+
+  if (kubeconfig) {
+    core.info(`Applying kubeconfig`)
+
+    if (!existsSync('.kube')) {
+      mkdirSync('.kube')
+    }
+
+    const path = resolve('.kube/config')
+
+    core.info(`Wrote kubeconfig to: ${path}`)
+    writeFileSync(path, kubeconfig)
+    process.env.KUBECONFIG = path
+    core.exportVariable('KUBECONFIG', path)
+  } else if (core.getBooleanInput('k3s-install')) {
     core.info('Installing k3s')
     const containerName = await k3s.install(core.getInput('k3s-version'))
     core.saveState('containerName', containerName)
